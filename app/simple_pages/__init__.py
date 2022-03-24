53dcf2fe8e9aa48fa282b4b523a7c72eea6086b0
rod608 <rod608ney@gmail.com>
2022-03-24 13:03:06 -0400

    - Cloned project, personalized secrets.

--- app/simple_pages/__init__.py
+++ app/simple_pages/__init__.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-simple_pages = Blueprint('simple_pages', __name__,
-                        template_folder='templates')
+simple_pages = Blueprint('simple_pages', __name__, template_folder='templates')
 
 
 @simple_pages.route('/')
